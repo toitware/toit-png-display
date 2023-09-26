c26137ce12a8bc86cc09b15e94bd9837cab7f708
Erik Corry <erik@toitware.com>
2023-09-26 12:30:33 +0200

    Run tests from top level directory

--- tests/run-test.sh
+++ tests/run-test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mkdir -p out
+mkdir -p tests/out
 
 TOIT_EXE=$1
 TOIT_FILE=$2
@@ -10,6 +10,6 @@ TOIT_FILE=$2
 # Replace .toit with .png:
 OUTPUT_FILE=${TOIT_FILE%.toit}.png
 
-$TOIT_EXE $TOIT_FILE out/$OUTPUT_FILE
+$TOIT_EXE tests/$TOIT_FILE tests/out/$OUTPUT_FILE
 
-cmp out/$OUTPUT_FILE gold/$OUTPUT_FILE
+cmp tests/out/$OUTPUT_FILE tests/gold/$OUTPUT_FILE
